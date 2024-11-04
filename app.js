8213ba4a737b431da82add23d0d1df0e1369e771
EWART-boop <johnewartdelacruz@gmail.com>
2024-11-04 19:53:54 +0800

    profile.js

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let userProfile = {
+    fullName: "Grace sayan",  
+    age: 21,               
+    favoriteNumber: 27,     
+    favoriteColors: ["red", "violet", "pink"] 
+};
+
+
+console.log(userProfile);
